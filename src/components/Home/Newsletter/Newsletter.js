bfaed218f96ef4ee9a857469048c744c24a7420c
MunnaAhmed130 <munnaahmed2025@gmail.com>
2021-11-13 21:14:16 +0600

    commit

--- src/components/Home/Newsletter/Newsletter.js
+++ src/components/Home/Newsletter/Newsletter.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import './Newsletter.css'
+import './Newsletter.css';
+
+
 const Newsletter = () => {
+
     return (
         <div className="newsletter">
             <div className="newsletter-info">
                 <h2>SUBSCRIBE OUR NEWLETTER</h2> <br />
                 <h3>GET UPDATE FOR NEWS, OFFERS</h3> <br />
-                <input type="email" placeholder="Email" />
+                <form ><input type="email" placeholder="Email" /></form>
             </div>
 
         </div>
