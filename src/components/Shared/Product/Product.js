4d493d50801de387119ef615a3420bea767dd5e1
Munna Ahmed <munnaahmed2025@gmail.com>
2022-08-29 18:15:52 +0600

    added discounted price

--- src/components/Shared/Product/Product.js
+++ src/components/Shared/Product/Product.js
@@ -9,7 +9,9 @@ const Product = ({ product }) => {
   const { name, img, _id, rating, discount, price } = product;
   const [value, setValue] = React.useState(rating);
   // const discount = -20;
-
+  const discountPrice = (price * discount) / 100;
+  const discountedPrice = price - discountPrice;
+  let viewportWidth = window.innerWidth;
   // if (!product.length) {
   //   <CircularProgress />;
   // }
@@ -20,7 +22,7 @@ const Product = ({ product }) => {
         {/* {img.length && ( */}
         <div className="card_img_container">
           <Card.Img variant="top" src={img} className="card-img" />
-          {discount && <span className="discount_ribbon">{discount}%</span>}
+          {discount && <span className="discount_ribbon">- {discount}%</span>}
         </div>
 
         {/* )} */}
@@ -36,7 +38,16 @@ const Product = ({ product }) => {
             emptyIcon={<StarIcon className="empty_rating" />}
             readOnly
           />
-          <h6>Price: &#x24;{price}</h6>
+          {!discount ? (
+            <p className="card_price">Price: &#x24;{price}</p>
+          ) : (
+            <p className="card_price">
+              Price: &#x24;
+              {discountedPrice}&nbsp;
+              <del className="delete-text">&#x24;{price}</del>
+              {viewportWidth < 300 && <br />}
+            </p>
+          )}
           <Link className="dynamic-route" to={`/purchase/${_id}`}>
             <Button size="large" variant="contained" className="buy-btn">
               Add To Cart
