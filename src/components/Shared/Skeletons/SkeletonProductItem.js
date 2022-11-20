6e7e418d7d351b50324f128eaf8a9c8ed7205ce1
Munna Ahmed <munnaahmed2025@gmail.com>
2022-11-21 02:28:21 +0600

    explore item loading skeleton responsive

--- src/components/Shared/Skeletons/SkeletonProductItem.js
+++ src/components/Shared/Skeletons/SkeletonProductItem.js
@@ -10,10 +10,10 @@ const SkeletonProductItem = () => {
 
   return (
     <div className={` explore-item--skeleton explore-item product_item`}>
-      <Container className="skeleton_item_container " fluid>
+      <Container className="skeleton_item_container item__container" fluid>
         <Row xs={1} sm={1} md={2} lg={2} xl={2} xxl={2} className=" item_row">
           <Col md={12} lg={4} xl={4} xxl={4} className="item_img_container  ">
-            <SkeletonElements type="product_img">
+            <SkeletonElements type="product_img item__img">
               {/* <Shimmer></Shimmer> */}
             </SkeletonElements>
           </Col>
@@ -25,24 +25,15 @@ const SkeletonProductItem = () => {
             className="skeleton_item_info_container"
           >
             <SkeletonElements type="title" />
-            {viewportWidth > 320 ? (
-              <div className="skeleton_item_description">
-                <SkeletonElements type="text" />
-                <SkeletonElements type="text" />
-                <SkeletonElements type="text" />
-              </div>
-            ) : (
-              <div className="skeleton_item_description">
-                <SkeletonElements type="text" />
-                <SkeletonElements type="text" />
-                <SkeletonElements type="text" />
-                <SkeletonElements type="text" />
-                <SkeletonElements type="text" />
-              </div>
-            )}
+            <div className="skeleton__item__description item__description">
+              <SkeletonElements type="text" />
+              <SkeletonElements type="text" />
+              <SkeletonElements type="text" />
+              <SkeletonElements type="text" />
+            </div>
             <SkeletonElements type="price" />
             <SkeletonElements type="rating" />
-            <div className="skeleton_btn_container">
+            <div className="skeleton__btn__container">
               <SkeletonElements type="button" />
               <SkeletonElements type="button" />
             </div>
